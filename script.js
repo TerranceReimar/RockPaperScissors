9dec9afca8f89de6837ae5f61fa9dd877407abcd
TerranceReimar <terrancereimar@gmail.com>
2022-08-28 11:24:56 +0200

    Added a stylesheet and changed the JS script to add divs for respectable Rock, Paper and Scissor buttons

--- script.js
+++ script.js
@@ -1,55 +1,18 @@
-let playerScore = 0;
-let computerScore = 0;
+const container = document.querySelector("#container");
+const rock = document.createElement("div");
+const paper = document.createElement("div");
+const scissors = document.createElement("div");
 
-function getComputerChoice(computerChoice) {
-  let rockPaperScissors = ["Rock", "Paper", "Scissors"];
-  computerChoice =
-    rockPaperScissors[Math.floor(Math.random() * rockPaperScissors.length)];
-  return computerChoice;
-}
+rock.setAttribute("style", "Background-color: yellow; border: 2px solid black");
+paper.setAttribute(
+  "style",
+  "Background-color: yellow; border: 2px solid black"
+);
+scissors.setAttribute(
+  "style",
+  "Background-color: yellow; border: 2px solid black"
+);
 
-function playRound(computerSelection, playerSelection) {
-  playerSelection = prompt("Enter Rock/Paper/Scissors");
-  playerSelection = `${playerSelection
-    .slice(0, 1)
-    .toUpperCase()}${playerSelection.slice(1).toLowerCase()}`;
-
-  computerSelection = getComputerChoice();
-  console.log(playerSelection);
-  console.log(computerSelection);
-
-  if (computerSelection == playerSelection) {
-    alert("It's a tie!");
-  } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
-    alert("You win, Rock beats Scissors");
-    playerScore++;
-  } else if (playerSelection == "Paper" && computerSelection == "Rock") {
-    alert("You win, Paper beats Rock");
-    playerScore++;
-  } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
-    alert("You win, Scissors beats Paper");
-    playerScore++;
-  } else {
-    alert(`You lose, ${computerSelection} beats ${playerSelection}`);
-    computerScore++;
-  }
-}
-
-function game() {
-  for (let i = 0; i < 5; i++) {
-    playRound();
-  }
-  if (computerScore == playerScore) {
-    alert("It's a tie game!");
-  } else if (playerScore > computerScore) {
-    alert(
-      `You win, Computer score ${computerScore} - Player score ${playerScore}`
-    );
-  } else {
-    alert(
-      `Computer wins, Computer score ${computerScore} - Player Score ${playerScore}`
-    );
-  }
-}
-
-game();
+container.appendChild(rock);
+container.appendChild(paper);
+container.appendChild(scissors);
