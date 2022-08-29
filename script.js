d16abadd45b3d3cb30e6de22f4f110fb122d7f93
TerranceReimar <terrancereimar@gmail.com>
2022-08-29 02:09:16 +0200

    Added GUI to the rockPaperScissors game

--- script.js
+++ script.js
@@ -1,18 +1,62 @@
-const container = document.querySelector("#container");
-const rock = document.createElement("div");
-const paper = document.createElement("div");
-const scissors = document.createElement("div");
+const buttons = document.querySelector("#buttons");
+const header = document.querySelector("header");
 
-rock.setAttribute("style", "Background-color: yellow; border: 2px solid black");
-paper.setAttribute(
-  "style",
-  "Background-color: yellow; border: 2px solid black"
-);
-scissors.setAttribute(
-  "style",
-  "Background-color: yellow; border: 2px solid black"
-);
+const title = document.createElement("h1");
+const desc = document.createElement("p");
+title.textContent = "Welcome to Rock, Papaer, Scissors!";
+desc.textContent = "Click on one of the buttons to start.";
+header.appendChild(title);
+header.appendChild(desc);
 
-container.appendChild(rock);
-container.appendChild(paper);
-container.appendChild(scissors);
+// Insert button elements (rock, paper, scissors)
+const rock = document.createElement("button");
+const paper = document.createElement("button");
+const scissors = document.createElement("button");
+
+// Insert text and logo's to the buttons
+rock.textContent = "Rock";
+paper.textContent = "Paper";
+scissors.textContent = "Scissors";
+
+// Adding Classes to style with CSS
+rock.classList.add("rock");
+paper.classList.add("paper");
+scissors.classList.add("scissors");
+
+// Adding elements to HTML document
+buttons.appendChild(rock);
+buttons.appendChild(paper);
+buttons.appendChild(scissors);
+
+function playGame(userGuess, computerGuess) {
+  const items = ["Rock", "Paper", "Scissors"];
+  computerGuess = items[Math.floor(Math.random() * items.length)];
+
+  if (userGuess == computerGuess) {
+    outcome.textContent = "It's a tie!";
+  } else if (userGuess == "Rock" && computerGuess == "Scissors") {
+    outcome.textContent = "You win!";
+  } else if (userGuess == "Paper" && computerGuess == "Rock") {
+    outcome.textContent = "You win!";
+  } else if (userGuess == "Scissors" && computerGuess == "Paper") {
+    outcome.textContent = "You win!";
+  } else {
+    outcome.textContent = "You lose.";
+  }
+}
+
+rock.addEventListener("click", function () {
+  playGame("Rock");
+});
+paper.addEventListener("click", function () {
+  playGame("Paper");
+});
+scissors.addEventListener("click", function () {
+  playGame("Scissors");
+});
+
+// Div that tells the user the outcome
+const outcome = document.querySelector("#outcome");
+const outcomeText = document.createElement("p");
+outcomeText.classList.add("outcomeText");
+outcome.appendChild(outcomeText);
