74dd45ee5b94076b433ad3f6da188192030063fe
TerranceReimar <terrancereimar@gmail.com>
2022-11-19 13:37:24 +0200

    Rock Paper Scissors game fully functional and ready to commit

--- script.js
+++ script.js
@@ -1,65 +1,78 @@
-const rock = document.querySelector(".rock");
-const papper = document.querySelector(".paper");
-const scissors = document.querySelector(".scissors");
-const result = document.querySelector("#result");
-
-// Display the running score
 let userScore = 0;
 let computerScore = 0;
-const displayUserScore = document.querySelector("#displayUserScore");
-const displayComputerScore = document.querySelector("#displayComputerScore");
-const getUserScore = document.createElement("p");
-const getComputerScore = document.createElement("p");
-displayUserScore.appendChild(getUserScore);
-displayComputerScore.appendChild(getComputerScore);
-getUserScore.textContent = userScore;
-getComputerScore.textContent = computerScore;
 
-function checkWinner() {
-  if (userScore == 5 && userScore > computerScore) {
-    result.textContent  = "You beat the computer, good job!";
-  }
-  if (computerScore == 5 && computer < userScore) {
-    result.textContent = "The computer won, better luck next time...";
-  }
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+
+const result = document.querySelector(".display-result");
+const score = document.querySelector(".display-score");
+
+function getRandom(computerGuess) {
+  const choiceList = ["Rock", "Paper", "Scissors"];
+  computerGuess = choiceList[Math.floor(Math.random() * choiceList.length)];
+  return computerGuess.toUpperCase();
 }
 
-function playRound(userGuess, computerGuess) {
-  const items = ["Rock", "Paper", "Scissors"];
-  computerGuess = items[Math.floor(Math.random() * items.length)];
+let userGuess = "";
+
+rock.onclick = () => {
+  userGuess = "ROCK";
+  playRound();
+  checkScore();
+};
+
+paper.onclick = () => {
+  userGuess = "PAPER";
+  playRound();
+  checkScore();
+};
+
+scissors.onclick = () => {
+  userGuess = "SCISSORS";
+  playRound();
+  checkScore();
+};
+
+function playRound(computerGuess) {
+  computerGuess = getRandom();
 
   if (userGuess == computerGuess) {
-    result.textContent = "It's a tie";
-  } else if (userGuess == "Rock" && computerGuess == "Scissors") {
+    result.textContent = `You picked ${userGuess} and the computer picked ${computerGuess} 
+    It's a draw, keep going.`;
+  } else if (userGuess == "ROCK" && computerGuess == "SCISSORS") {
+    result.textContent = `You picked ${userGuess} and the computer picked ${computerGuess} 
+    You won!`;
     userScore++;
-    getUserScore.textContent = userScore;
-    result.textContent = "Rock beats Scissors, You win!";
-    return userScore;
-  } else if (userGuess == "Paper" && computerGuess == "Rock") {
+  } else if (userGuess == "PAPER" && computerGuess == "ROCK") {
+    result.textContent = `You picked ${userGuess} and the computer picked ${computerGuess} 
+    You won!`;
     userScore++;
-    getUserScore.textContent = userScore;
-    result.textContent = "Paper breats Rock, You win!";
-    return userScore;
-  } else if (userGuess == "Scissors" && computerGuess == "Paper") {
+  } else if (userGuess == "SCISSORS" && computerGuess == "PAPER") {
+    result.textContent = `You picked ${userGuess} and the computer picked ${computerGuess} 
+    You won!`;
     userScore++;
-    getUserScore.textContent = userScore;
-    result.textContent = "Scissors beats Paper, You win!";
-    return userScore;
   } else {
+    result.textContent = `You picked ${userGuess} and the computer picked ${computerGuess} 
+    You lost.`;
     computerScore++;
-    getComputerScore.textContent = computerScore;
-    result.textContent = `${computerGuess} beats ${userGuess}, You lose...`;
-    return computerScore;
   }
 }
-rock.addEventListener("click", function () {
-  playRound("Rock");
-});
-papper.addEventListener("click", function () {
-  playRound("Paper");
-});
-scissors.addEventListener("click", function () {
-  playRound("Scissors");
-});
 
-
+function checkScore() {
+  if (userScore == 5 || computerScore == 5) {
+    if (userScore > computerScore) {
+      result.textContent = `You beat the computer, well done!`;
+      result.setAttribute("style", "font-weight: bold;");
+    } else if (computerScore > userScore) {
+      result.textContent = `The computer won the game, better luck next time.`;
+      result.setAttribute("style", "font-weight: bold;");
+    }
+    userScore = 0;
+    computerScore = 0;
+    score.textContent = "Click game cards to restart.";
+  } else if (userScore < 5 || computerScore < 5) {
+    score.textContent = `User score : ${userScore} Computer score : ${computerScore}`;
+    result.setAttribute("style", "font-weight: none;");
+  }
+}
